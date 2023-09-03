1447f8d8c53d8b0ba44556ac807a1cbcd0e0c7a3
Ernani Brandao <ernani.brandao@gmail.com>
2023-09-03 12:42:56 -0300

    tst

--- lotto/src/main/java/br/ejcb/app/LottoApplication.java
+++ lotto/src/main/java/br/ejcb/app/LottoApplication.java
@@ -1,13 +1,21 @@
 package br.ejcb.app;
 
-import io.quarkus.runtime.Quarkus;
-import io.quarkus.runtime.annotations.QuarkusMain;
+import br.ejcb.app.command.CriarJogoCommand;
+import br.ejcb.app.command.ListarJogosCommand;
+import io.quarkus.picocli.runtime.annotations.TopCommand;
+import picocli.CommandLine;
 
-@QuarkusMain
+@TopCommand
+@CommandLine.Command(name = "lotto-app", 
+		mixinStandardHelpOptions = true, 
+		subcommands = {CriarJogoCommand.class, ListarJogosCommand.class}
+)
 public class LottoApplication {
 
     public static void main(String[] args) {
-    	Quarkus.run(args);
+        int exitCode = new CommandLine(new LottoApplication()).execute(args);
+        System.exit(exitCode);
     }
 
 }
+
