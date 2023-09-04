313da3979678c7e45120a9bf95adda44fd0f973a
Ernani Brandao <ernani.brandao@gmail.com>
2023-09-03 21:01:59 -0300

    retiara lotto msena quarkus

--- lotto/src/test/java/br/ejcb/app/LottoApplicationIT.java
+++
@@ -1,8 +0,0 @@
-package br.ejcb.app;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class LottoApplicationIT extends LottoApplicationTest {
-    // Execute the same tests but in packaged mode.
-}
